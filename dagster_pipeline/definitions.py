9e5c2ca09d6438c14eb69fc0690ce78387b38adb
Jhonny <ramirezjhonny64@gmail.com>
2025-10-17 13:45:39 -0500

    feat:adding prueba file asset and duckdb_connection resources

--- dagster_pipeline/definitions.py
+++ dagster_pipeline/definitions.py
@@ -5,24 +5,29 @@ into objects Dagster can discover via `workspace.yaml`.
 """
 
 import os
-from dagster import Definitions
-from dagster_pipeline.assets.google_drive_etl import process_drive_files
-from dagster_pipeline.resources import GoogleDriveResource
+from dagster_pipeline.assets import google_drive_etl, prueba
+from dagster_pipeline.resources import GoogleDriveResource, database_resource
 from dagster_pipeline.sensors import google_drive_new_file_sensor
 from config import CREDENTIALS_PATH, TOKEN_PATH
+#definitions_prueba
+from dagster import Definitions, load_assets_from_modules
+from dagster_pipeline.resources.duckdb_connection import database_resource
+
 
 # Get the project root directory (parent of pipeline directory)
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+all_assets=load_assets_from_modules([prueba,google_drive_etl])
 
 # Create the Definitions object - Dagster will discover this
 defs = Definitions(
-    assets=[process_drive_files],
+    assets=all_assets,
     resources={
         "google_drive": GoogleDriveResource(
             credentials_path=str(CREDENTIALS_PATH),
             token_path=str(TOKEN_PATH),
             scopes=["https://www.googleapis.com/auth/drive"]
-        )
+        ),
+        "duckdb":database_resource,
     },
     sensors=[google_drive_new_file_sensor],
 )
